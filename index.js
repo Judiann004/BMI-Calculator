c3d3427488764c3f027e3f203149520ba870e73e
Judiann <111895101+Judiann004@users.noreply.github.com>
2023-03-24 09:09:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function calculateBMI() {
   const heightValue = document.getElementById("height").value / 100;
   const weightValue = document.getElementById("weight").value;
 
-  const bmiValue = weightValue / (heightValue * heightValue);
+  const bmiValue = Math.round(weightValue / (heightValue * heightValue));
 
   bmiInputEl.value = bmiValue;
 
